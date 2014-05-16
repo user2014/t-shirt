a346bd19fd875787f4120636a2d8b3ee87aef60b
Gergely Daróczi <daroczig@rapporter.net>
2014-05-16 23:02:18 +0200

    negative

--- tshirt_code.R
+++ tshirt_code.R
@@ -2,6 +2,6 @@ library(pixmap)
 R <- read.pnm(system.file('pictures/logo.ppm', package = 'pixmap')[1])
 R <- addChannels(R)@grey
 T <- matrix(strsplit('useR!2014', '')[[1]], nrow = 77, ncol = 101, byrow = TRUE)
-T[which(R < 0.75)] <- ' '
+T[which(R > 0.75)] <- ' '
 write.table(T, 'tshirtImage.txt', row.names = FALSE,
             sep = '', col.names = FALSE, quote = FALSE)
