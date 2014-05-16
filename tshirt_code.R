570430cc9bd5243507c2521436a5c62519da3580
Jim Hester <james.f.hester@gmail.com>
2014-05-16 10:46:27 -0400

    Update tshirt_code.R
    - recycled lengths rather than repeated calls to rep
    - formatted lens specification by row
    - use strsplit of character string rather than explicit declaration of split characters
    - specify byrow in matrix construction rather than transposing post creation
    - use spaces for column names rather than dots
    - print without row names

--- tshirt_code.R
+++ tshirt_code.R
@@ -1,24 +1,34 @@
-R<-c(rep(1,19),rep(0,5), rep(1,20),rep(0,4),
-     rep(1,22),rep(0,2),rep(1,6),rep(0,10),rep(1,7),0,
-     rep(1,6),rep(0,11),rep(1,7),rep(1,6),rep(0,12),
-     rep(1,6),rep(1,6),rep(0,12),rep(1,6), rep(1,6),
-     rep(0,12),rep(1,6),rep(1,6),rep(0,12),rep(1,6),
-     rep(1,6),rep(0,10),rep(1,7),0,rep(1,23),0,
-     rep(1,22),rep(0,2),rep(1,21),rep(0,3),rep(1,19),
-     rep(0,5),rep(1,6),rep(0,7),rep(1,5),rep(0,6),
-     rep(1,6),rep(0,8),rep(1,4),rep(0,6),rep(1,6),
-     rep(0,8),rep(1,5),rep(0,5),rep(1,6),rep(0,9),
-     rep(1,4),rep(0,5),rep(1,6),rep(0,9),rep(1,5),
-     rep(0,4),rep(1,6),rep(0,10),rep(1,4),rep(0,4),
-     rep(1,6),rep(0,10),rep(1,5),rep(0,3),rep(1,6),
-     rep(0,11),rep(1,5),rep(0,2),rep(1,6),rep(0,11),
-     rep(1,6),rep(0,1),rep(1,6),rep(0,12),rep(1,6))
-R2<-rep(c('u','s','e','R','!','2','0','1','4'),64)
-R <- ifelse(as.logical(R), R2, "")
+lens = c(19,5,
+        20,4,
+        22,2,
+        6,10,7,1,
+        6,11,7,0,
+        6,12,6,0,
+        6,12,6,0,
+        6,12,6,0,
+        6,12,6,0,
+        6,10,7,1,
+        23,1,
+        22,2,
+        21,3,
+        19,5,
+        6,7,5,6,
+        6,8,4,6,
+        6,8,5,5,
+        6,9,4,5,
+        6,9,5,4,
+        6,10,4,4,
+        6,10,5,3,
+        6,11,5,2,
+        6,11,6,1,
+        6,12,6,0)
+R<-rep(rep(c(TRUE,FALSE), length.out=length(lens)), times=lens)
+R2<-rep(strsplit('useR12014', '')[[1]],64)
+R <- ifelse(R, R2, "")
 
-R[265:288]<-c('u','s','e','R','!','2','0','1','4','-','-',
-              'L','o','s',' ','A','n','g','e','l','e','s','','')
-R<-as.data.frame(t(matrix(R,ncol=24)))
-names(R)<-rep('.',24)
-R
+R[265:286]<-strsplit('user!2014--Los Angeles', '')[[1]]
+
+R<-data.frame(matrix(R,ncol=24, byrow=T))
+names(R) = rep(' ', ncol(R))
+print(R, row.names=F)
 # write.table(R, file="tshirtImage.txt", quote=FALSE)
