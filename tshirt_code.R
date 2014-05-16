2815a42cc48ae631f0440435a6a2e9161fdb5eda
Gergely Daróczi <daroczig@rapporter.net>
2014-05-16 22:44:49 +0200

    using pixmap to create ASCII art

--- tshirt_code.R
+++ tshirt_code.R
@@ -1,34 +1,13 @@
-lens = c(19,5,
-        20,4,
-        22,2,
-        6,10,7,1,
-        6,11,7,0,
-        6,12,6,0,
-        6,12,6,0,
-        6,12,6,0,
-        6,12,6,0,
-        6,10,7,1,
-        23,1,
-        22,2,
-        21,3,
-        19,5,
-        6,7,5,6,
-        6,8,4,6,
-        6,8,5,5,
-        6,9,4,5,
-        6,9,5,4,
-        6,10,4,4,
-        6,10,5,3,
-        6,11,5,2,
-        6,11,6,1,
-        6,12,6,0)
-R<-rep(rep(c(TRUE,FALSE), length.out=length(lens)), times=lens)
-R2<-rep(strsplit('useR12014', '')[[1]],64)
-R <- ifelse(R, R2, "")
-
-R[265:286]<-strsplit('user!2014--Los Angeles', '')[[1]]
-
-R<-data.frame(matrix(R,ncol=24, byrow=T))
-names(R) = rep(' ', ncol(R))
-print(R, row.names=F)
-# write.table(R, file="tshirtImage.txt", quote=FALSE)
+library(pixmap)
+R <- read.pnm(system.file('pictures/logo.ppm', package = 'pixmap')[1])
+R <- addChannels(R)@grey
+R <- matrix(
+    as.character(
+        cut(
+            x      = R,
+            breaks = c(-Inf, 0.25, 0.5, 0.75, 0.9, Inf),
+            labels = c('#', 'x', 'o', '.', ' '),
+            include.lowest = TRUE)),
+    nrow = 77)
+write.table(R, 'tshirtImage.txt', row.names = FALSE,
+            sep = '', col.names = FALSE, quote = FALSE)
