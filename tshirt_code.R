f21c90fa49f04105732f9930ea8848ff06ace7bd
Gergely Daróczi <daroczig@rapporter.net>
2014-05-16 22:57:31 +0200

    "useR! 2014" instead of ASCII art chars

--- tshirt_code.R
+++ tshirt_code.R
@@ -1,13 +1,9 @@
 library(pixmap)
 R <- read.pnm(system.file('pictures/logo.ppm', package = 'pixmap')[1])
 R <- addChannels(R)@grey
-R <- matrix(
-    as.character(
-        cut(
-            x      = R,
-            breaks = c(-Inf, 0.25, 0.5, 0.75, 0.9, Inf),
-            labels = c('#', 'x', 'o', '.', ' '),
-            include.lowest = TRUE)),
-    nrow = 77)
+R <- substr(as.character(R < 0.75), 1, 1)
+R <- matrix(R, nrow = 77)
+R[which(R == 'F')] <- ' '
+R[which(R == 'T', arr.ind = TRUE)] <- strsplit('useR!2014', '')[[1]]
 write.table(R, 'tshirtImage.txt', row.names = FALSE,
             sep = '', col.names = FALSE, quote = FALSE)
