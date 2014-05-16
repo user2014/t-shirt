533f94feff53ee844023a463cd4ac6f293d0d062
Gergely Daróczi <daroczig@rapporter.net>
2014-05-16 23:01:18 +0200

    simplified code

--- tshirt_code.R
+++ tshirt_code.R
@@ -1,9 +1,7 @@
 library(pixmap)
 R <- read.pnm(system.file('pictures/logo.ppm', package = 'pixmap')[1])
 R <- addChannels(R)@grey
-R <- substr(as.character(R < 0.75), 1, 1)
-R <- matrix(R, nrow = 77)
-R[which(R == 'F')] <- ' '
-R[which(R == 'T', arr.ind = TRUE)] <- strsplit('useR!2014', '')[[1]]
-write.table(R, 'tshirtImage.txt', row.names = FALSE,
+T <- matrix(strsplit('useR!2014', '')[[1]], nrow = 77, ncol = 101)
+T[which(R < 0.75)] <- ' '
+write.table(T, 'tshirtImage.txt', row.names = FALSE,
             sep = '', col.names = FALSE, quote = FALSE)
