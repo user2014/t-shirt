d3242ffa1986377b781f0e52d1e4b395dd250dcc
Gergely Daróczi <daroczig@rapporter.net>
2014-05-17 15:47:11 +0200

    break lines more often to fit on the back of a t-shirt

--- tshirt_code.R
+++ tshirt_code.R
@@ -1,7 +1,17 @@
 library(pixmap)
-R <- read.pnm(system.file('pictures/logo.ppm', package = 'pixmap')[1])
+R <- read.pnm(
+    system.file('pictures/logo.ppm',
+                package = 'pixmap')[1])
 R <- addChannels(R)@grey
-T <- matrix(strsplit('useR!2014', '')[[1]], nrow = 77, ncol = 101, byrow = TRUE)
+T <- matrix(
+    strsplit('useR!2014', '')[[1]],
+    nrow = 77,
+    ncol = 101,
+    byrow = TRUE)
 T[which(R > 0.85)] <- ' '
-write.table(T, 'tshirtImage.txt', row.names = FALSE,
-            sep = '', col.names = FALSE, quote = FALSE)
+write.table(x = T,
+            file = 'tshirtImage.txt',
+            row.names = FALSE,
+            sep = '',
+            col.names = FALSE,
+            quote = FALSE)
