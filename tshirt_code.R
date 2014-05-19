5ccf0c0be1c9190426a916b6bb2e23e879b0065a
AmeliaMN <amelia.a.mcnamara@gmail.com>
2014-05-19 12:25:21 -0700

    Revert c6d9a1a..46d5364
    This rolls back to commit c6d9a1aa7a637127a7d2ffdd32a38f0d26c8895a.

--- tshirt_code.R
+++ tshirt_code.R
@@ -1,35 +1,34 @@
-# jo f. with thanks to:
-# http://is-r.tumblr.com/post/46821313005/to-plot-them-is-my-real-test
-# http://stackoverflow.com/questions/12918367/in-r-how-to-plot-with-a-png-as-background
-# http://students.washington.edu/mclarkso/documents/figure%20layout%20Ver1.R
-# http://georeferenced.wordpress.com/2013/01/15/rwordcloud/
+lens = c(19,5,
+        20,4,
+        22,2,
+        6,10,7,1,
+        6,11,7,0,
+        6,12,6,0,
+        6,12,6,0,
+        6,12,6,0,
+        6,12,6,0,
+        6,10,7,1,
+        23,1,
+        22,2,
+        21,3,
+        19,5,
+        6,7,5,6,
+        6,8,4,6,
+        6,8,5,5,
+        6,9,4,5,
+        6,9,5,4,
+        6,10,4,4,
+        6,10,5,3,
+        6,11,5,2,
+        6,11,6,1,
+        6,12,6,0)
+R<-rep(rep(c(TRUE,FALSE), length.out=length(lens)), times=lens)
+R2<-rep(strsplit('useR12014', '')[[1]],64)
+R <- ifelse(R, R2, "")
 
-rm(list=ls())
-sapply(c("stringr", "jpeg", "RCurl", "EBImage", "wordcloud", "tm"),library, character.only=TRUE)
-allImageURLs <- c("http://upload.wikimedia.org/wikipedia/commons/c/c1/Rlogo.png",
-				  "http://www.memes.at/faces/cereal_guy_squint.jpg",
-				  "http://img2.wikia.nocookie.net/__cb20120912234733/ragecomic/images/9/91/Cereal_Guy_Spitting.jpeg")
-imageList <- list()
-for(imageURL in allImageURLs) {
-	print(imageURL)
-   tempName <- str_extract(imageURL,"([[:alnum:]_-]+)([[:punct:]])([[:alnum:]]+)$")
-   print(tempName)
-   tempImage <- readImage(imageURL) 
-   imageList[[tempName]] <- tempImage 
-}
-par(mfrow=c(2,2))	
-plot(0:10, 0:10, type="n", axes=F, ann=FALSE)
-rasterImage(imageList[[1]],1,1,10,10)
-box("figure", col="black", lwd=2)
-plot(0:10, 0:10, type="n", axes=F, ann=FALSE)
-rasterImage(imageList[[2]],1,1,10,10)
-box("figure", col="black", lwd=2)
-useR <- Corpus (DirSource("./useRdir"))
-useR <- tm_map(useR, stripWhitespace)
-useR <- tm_map(useR, tolower)
-useR <- tm_map(useR, removeWords, stopwords('english'))
-wordcloud(useR, scale=c(4,1.25), max.words=100, random.order=FALSE, rot.per=0.35, use.r.layout=FALSE, colors=brewer.pal(8,'Dark2'))
-box("figure", col="black", lwd=2)
-plot(0:10, 0:10, type="n", axes=F, ann=FALSE)
-rasterImage(imageList[[3]],1,1,10,10)
-box("figure", col="black", lwd=2)
+R[265:286]<-strsplit('user!2014--Los Angeles', '')[[1]]
+
+R<-data.frame(matrix(R,ncol=24, byrow=T))
+names(R) = rep(' ', ncol(R))
+print(R, row.names=F)
+# write.table(R, file="tshirtImage.txt", quote=FALSE)
